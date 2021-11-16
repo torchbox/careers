0108341d6040e4fcc0e153dbad2e6e4bc647295d
Chris Lawton <chris.lawton@torchbox.com>
2021-11-16 09:23:07 +0000

    add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel='preload'
+          href='/fonts/apercu/apercu-light-pro.woff2'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/apercu/apercu-regular-pro.woff2'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/apercu/apercu-bold-pro.woff2'
+          as='font'
+          crossOrigin=''
+        />
+        <link
+          rel='preload'
+          href='/fonts/apercu/apercu-black-pro.woff2'
+          as='font'
+          crossOrigin=''
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
