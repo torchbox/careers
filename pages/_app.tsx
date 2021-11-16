3c5ff2db91887d64ad35473d84dc0496a0a2f241
Chris Lawton <chris.lawton@torchbox.com>
2021-11-16 10:30:37 +0000

    lint after adding editor config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,40 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link
-          rel='preload'
-          href='/fonts/apercu/apercu-light-pro.woff2'
-          as='font'
-          crossOrigin=''
-        />
-        <link
-          rel='preload'
-          href='/fonts/apercu/apercu-regular-pro.woff2'
-          as='font'
-          crossOrigin=''
-        />
-        <link
-          rel='preload'
-          href='/fonts/apercu/apercu-bold-pro.woff2'
-          as='font'
-          crossOrigin=''
-        />
-        <link
-          rel='preload'
-          href='/fonts/apercu/apercu-black-pro.woff2'
-          as='font'
-          crossOrigin=''
-        />
-      </Head>
+    return (
+        <>
+            <Head>
+                <link
+                    rel="preload"
+                    href="/fonts/apercu/apercu-light-pro.woff2"
+                    as="font"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/apercu/apercu-regular-pro.woff2"
+                    as="font"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/apercu/apercu-bold-pro.woff2"
+                    as="font"
+                    crossOrigin=""
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/apercu/apercu-black-pro.woff2"
+                    as="font"
+                    crossOrigin=""
+                />
+            </Head>
 
-      <Component {...pageProps} />
-    </>
-  );
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
