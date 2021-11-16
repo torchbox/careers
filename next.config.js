7622701b583d5899f7fa5a5c07b06aff3a026f96
Chris Lawton <chris.lawton@torchbox.com>
2021-11-16 10:32:06 +0000

    add sass and variables from main tbx site repo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    sassOptions: {
+        // allow all scss files access to these files
+        prependData: `@use "sass:math"; @import "variables.scss"; @import "functions.scss"; @import "fonts.scss"; `,
+    },
+};
