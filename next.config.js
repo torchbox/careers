331d1d96313398c7cf38000ae614d21765abb8a7
Chris Lawton <chris.lawton@torchbox.com>
2021-11-17 10:45:55 +0000

    make mixins available globally

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     reactStrictMode: true,
     sassOptions: {
         // allow all scss files access to these files
-        prependData: `@use "sass:math"; @import "variables.scss"; @import "functions.scss"; @import "fonts.scss"; `,
+        prependData: `@use "sass:math"; @import "variables.scss"; @import "functions.scss"; @import "mixins.scss"; @import "fonts.scss"; `,
     },
 };
