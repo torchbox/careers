59d31fc0d50885a03d0ecec694b394a8f15f0f25
Chris Lawton <chris.lawton@torchbox.com>
2021-11-19 16:22:51 +0000

    move the stub data to a folder

---
+++ data/benefits-list.js
@@ -0,0 +1,17 @@
+export const stubBenefitsData = [
+    {
+        text: "The opportunity to be a co-owner of Torchbox, shaping and sharing in our success",
+    },
+    {
+        text: "Private health scheme and access to mental health support",
+    },
+    {
+        text: "Ethical pension scheme, with a 5% contribution from Torchbox",
+    },
+    {
+        text: "25 days holiday, plus public holidays",
+    },
+    {
+        text: "etc - base on Benefits content type",
+    },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,7 @@
 import type { NextPage } from "next";
 import styles from "styles/Home.module.scss";
 import BenefitsList from "components/BenefitsList/BenefitsList";
-
-const stubBenefitsData = [
-    {
-        text: "The opportunity to be a co-owner of Torchbox, shaping and sharing in our success",
-    },
-    {
-        text: "Private health scheme and access to mental health support",
-    },
-    {
-        text: "Ethical pension scheme, with a 5% contribution from Torchbox",
-    },
-    {
-        text: "25 days holiday, plus public holidays",
-    },
-    {
-        text: "etc - base on Benefits content type",
-    },
-];
+import { stubBenefitsData } from "data/benefits-list";
 
 const Home: NextPage = () => {
     return (
