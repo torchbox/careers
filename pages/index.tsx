e0c038e6b96f662dbc82d185443e8fcbbf4ae27b
James Hancock <jhancock532@gmail.com>
2021-12-07 11:41:02 +0000

    Add landing page demonstation data display

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,49 @@
 import type { NextPage } from "next";
 import styles from "styles/Home.module.scss";
+import { getLandingPage } from "../lib/api";
+import Layout from "../components/Layout";
+import Image from "next/image";
 
-const Home: NextPage = () => {
+interface LandingPage {
+    title: string;
+    metadataDescription: string;
+    heroImage: HeroImage;
+}
+interface HeroImage {
+    description: string;
+    url: string;
+    width: number;
+    height: number;
+}
+
+const LandingPage: NextPage<{
+    preview: boolean;
+    landingPageContent: LandingPage;
+}> = ({ preview, landingPageContent }) => {
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Why Torchbox</h1>
-        </div>
+        <>
+            <Layout preview={preview}>
+                <div className={styles.container}>
+                    <h1 className={styles.title}>{landingPageContent.title}</h1>
+                    <Image
+                        src={landingPageContent.heroImage.url}
+                        alt={landingPageContent.heroImage.description}
+                        width={landingPageContent.heroImage.width}
+                        height={landingPageContent.heroImage.height}
+                    />
+                    <p>{landingPageContent.metadataDescription}</p>
+                </div>
+            </Layout>
+        </>
     );
 };
 
-export default Home;
+export default LandingPage;
+
+export async function getStaticProps({ preview = false }) {
+    console.log(preview);
+    const landingPageContent = (await getLandingPage(preview)) ?? [];
+    return {
+        props: { preview, landingPageContent },
+    };
+}
