110caebf4081d67323f28a70c1927688972f2af2
James Hancock <jhancock532@gmail.com>
2021-12-10 12:27:53 +0000

    install sentry (client errors won't log)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,15 @@ import styles from "styles/Home.module.scss";
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Why Torchbox</h1>
+            <h1 className={styles.title}>Why Torchbox?</h1>
+            <button
+                type="button"
+                onClick={() => {
+                    throw new Error("Sentry Frontend Error");
+                }}
+            >
+                Throw error
+            </button>
         </div>
     );
 };
