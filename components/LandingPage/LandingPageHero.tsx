7de3883d0f20816985d767b76a409f524a843b00
James Hancock <jhancock532@gmail.com>
2022-01-24 14:31:41 +0000

    add landing page hero component

---
+++ components/LandingPage/LandingPageHero.tsx
@@ -0,0 +1,30 @@
+import styles from "../../styles/Hero.module.scss";
+import { ImageTypes } from "../../types/Base";
+import { ShardClip } from './ShardClip';
+
+type LandingPageHeroProps = {
+    image: ImageTypes;
+    children: React.ReactNode;
+};
+
+export const LandingPageHero = ({ image, children }: LandingPageHeroProps) => {
+    return (
+        <div className={styles.heroContainer}>
+            <ShardClip>
+              { /* eslint-disable-next-line @next/next/no-img-element */ }
+              <img className={styles.image} src={image.url} alt={image.description} width={image.width} height={image.height}/>
+            </ShardClip>
+
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>
+                <span className={styles.titleAccent}>Being at</span><br/>
+                    Torchbox
+                </h1>
+                <div className="subtitle">{children}</div>
+            </div>
+
+        </div>
+    );
+};
+
+export default LandingPageHero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from 'next';
 import styles from 'styles/Home.module.scss';
 
+import LandingPageHero from 'components/LandingPage/LandingPageHero';
+
+const PlaceholderImage = {
+    description: 'This is a placeholder',
+    url: 'https://source.unsplash.com/random/900×700/?nature',
+    width: 750,
+    height: 750,
+};
+
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
+            <LandingPageHero image={PlaceholderImage}>
+                {/* Todo: Replace this with a rich text field component. */}
+                <p>
+                    <strong>Glad you asked.</strong> We are not your average
+                    digital agency...
+                </p>
+            </LandingPageHero>
             <h1 className={styles.title}>Vercel Setup</h1>
         </div>
     );
