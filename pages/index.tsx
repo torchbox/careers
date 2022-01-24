0d07fa9560b34f698fddf74f5b23947257a2d581
James Hancock <jhancock532@gmail.com>
2022-01-24 09:25:42 +0000

    vercel setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from 'styles/Home.module.scss';
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Why Torchbox</h1>
+            <h1 className={styles.title}>Vercel Setup</h1>
         </div>
     );
 };
