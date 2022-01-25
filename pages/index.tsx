f6f180b59f9ed6407da3ccbc15d89ef98ee65a23
James Hancock <jhancock532@gmail.com>
2022-01-25 15:22:55 +0000

    add CTA component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from 'next';
 import styles from 'styles/Home.module.scss';
 
+import LandingPageCTA from 'components/LandingPageCTA';
+
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Vercel Setup</h1>
+            <LandingPageCTA jobs={4} title="Ready to make a difference?">
+                <p>
+                    We’re always excited about finding new talent and meeting
+                    people that are as eager as we are to drive significant
+                    positive change.
+                </p>
+            </LandingPageCTA>
         </div>
     );
 };
