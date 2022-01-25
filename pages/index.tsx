04994f20612836938816e737e3bb3337c3787ea8
James Hancock <jhancock532@gmail.com>
2022-01-25 13:01:45 +0000

    add ComeWorkForYou component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from 'next';
 import styles from 'styles/Home.module.scss';
+import ComeWorkForYou from 'components/ComeWorkForYou';
+
+const PlaceholderImage = {
+    description: 'This is a placeholder',
+    url: 'https://source.unsplash.com/random/1600×900/?nature',
+    width: 1600,
+    height: 900,
+};
 
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Vercel Setup</h1>
+            <ComeWorkForYou image={PlaceholderImage}>
+                <strong>
+                    No parent company, no shareholders, just a team of equal
+                    owners.
+                </strong>
+                <br />
+                <br />
+                <p>
+                    100% of our business belongs to our Employee Ownership Trust
+                    (EOT). Everyone is included, everyone is updated and
+                    everyone belongs.
+                </p>
+            </ComeWorkForYou>
         </div>
     );
 };
---
+++ types/Base.ts
@@ -0,0 +1,6 @@
+export type ImageTypes = {
+    description: string;
+    url: string;
+    width: number;
+    height: number;
+};
