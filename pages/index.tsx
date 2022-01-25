a61948c81ba9d0abeb83e8aa25ef8182580d9bf4
James Hancock <jhancock532@gmail.com>
2022-01-25 17:20:21 +0000

    add life as a torchboxer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from 'next';
 import styles from 'styles/Home.module.scss';
 
+import LifeAsATorchboxer from 'components/LifeAsATorchboxer';
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Vercel Setup</h1>
+            <LifeAsATorchboxer>
+                <p>
+                    From Bristol to Texas, we go the extra mile (or 4,813).
+                    We’re remote-first, but always connected, championing unique
+                    people in unique places. <br />
+                    <br />
+                    Find out more about who we are, how we work, what we believe
+                    in and where we’re heading.
+                </p>
+            </LifeAsATorchboxer>
         </div>
     );
 };
