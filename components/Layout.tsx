30309a8cc95aa5694bbd14b2009aeb2085d25e9b
James Hancock <jhancock532@gmail.com>
2022-02-01 16:01:59 +0000

    add logo, update layout and types

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,17 @@
 import Alert from '../components/Alert';
+import Header from './Header';
+import { Theme } from '../types/Base';
 
 type LayoutProps = {
+    theme: Theme;
     preview: boolean;
     children: React.ReactNode;
 };
 
-export const Layout = ({ preview, children }: LayoutProps) => {
+export const Layout = ({ theme, preview, children }: LayoutProps) => {
     return (
         <div>
+            <Header theme={theme} />
             <Alert preview={preview} />
             <main>{children}</main>
         </div>
