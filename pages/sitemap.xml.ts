0d972204985f8d97a46b7ad1fa8daf3ce0ee415a
James Hancock <jhancock532@gmail.com>
2022-02-15 15:41:33 +0000

    update sitemap to pull correct urls for different sites

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,13 +1,19 @@
 import { GetServerSideProps } from 'next';
 
 function generateSiteMap() {
-    const rootUrl = process.env.VERCEL_URL
-        ? process.env.VERCEL_URL
-        : 'http://localhost:3000';
+    let baseUrl = 'http://localhost:3000';
+    if (process.env.VERCEL_ENV) {
+        baseUrl = {
+            production: 'https://theblackhart.co.uk',
+            preview: process.env.VERCEL_URL,
+            development: 'http://localhost:3000',
+        }[process.env.VERCEL_ENV] as string;
+    }
+
     return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
-       <loc>${rootUrl}</loc>
+       <loc>${baseUrl}</loc>
      </url>
    </urlset>
  `;
