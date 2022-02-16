0949778054fdbb1ed38ff4b8e276098e48846238
James Hancock <jhancock532@gmail.com>
2022-02-16 11:22:56 +0000

    add testimonial component to hero

--- pages/life-at-torchbox.tsx
+++ pages/life-at-torchbox.tsx
@@ -3,6 +3,7 @@ import styles from 'styles/LifeAtTorchbox.module.scss';
 import Layout from '../components/Layout';
 import { getLifeAtTorchboxPage } from '../lib/api';
 import { LifeAtTorchboxPage } from 'types/LifeAtTorchboxPage';
+import Testimonial from 'components/Testimonial';
 
 type LifeAtTorchboxPageProps = {
     preview: boolean;
@@ -17,18 +18,7 @@ const LifeAtTorchboxPage: NextPage<LifeAtTorchboxPageProps> = ({
         <div className={styles.indigoBackground}>
             <h1>{content.metadataTitle}</h1>
             <p>{content.metadataDescription}</p>
-
-            {/*
-            <Hero
-                image={PlaceholderImage}
-                video={PlaceholderVideo}
-                subtitle="Remote-first, but always connected, our international team’s a tight-knit bunch."
-            >
-                Our international clients are just a Zoom call away, and with a
-                solid team of 80+ permanent staff - spanning four offices and
-                five continents - we’ve got them covered, no matter the time
-                zone.
-          </Hero> */}
+            <Testimonial testimonial={content.itemsCollection.items[0]} />
         </div>
     </Layout>
 );
