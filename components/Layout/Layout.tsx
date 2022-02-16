cefdbf4e3d8efaf033b0305c90aa44c07c2c771b
James Hancock <jhancock532@gmail.com>
2022-02-16 11:22:00 +0000

    add testimonial clip path

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import Alert from '../Alert';
 import Footer from '../Footer';
 import Header from '../Header';
 import { Theme } from '../../types/Base';
+import styles from './Layout.module.scss';
 
 type LayoutProps = {
     theme: Theme;
@@ -29,8 +30,18 @@ export const Layout = ({
 
     return (
         <div className={themeClass}>
-            <Header jobsAvailable={jobsAvailable} />
+            <svg className={styles.svg}>
+                <defs>
+                    <clipPath
+                        id="testimonialClipPath"
+                        clipPathUnits="objectBoundingBox"
+                    >
+                        <path d="M 0.436 -0.001 L 0.937 0.258 L 0.572 0.997 L 0.062 0.746 Z" />
+                    </clipPath>
+                </defs>
+            </svg>
             <Alert preview={preview} />
+            <Header jobsAvailable={jobsAvailable} />
             <main>{children}</main>
             <Footer />
         </div>
