a29423f62a9b3e8872da9c9d832c5f3a27f866cf
James Hancock <jhancock532@gmail.com>
2022-02-21 09:14:21 +0000

    add job postings to sitemap

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,20 +1,44 @@
 import { GetServerSideProps } from 'next';
+import { getAllJobSlugs } from './api/jobs/slugs';
 
-function generateSiteMap() {
-    const rootUrl = process.env.VERCEL_URL
-        ? process.env.VERCEL_URL
-        : 'http://localhost:3000';
+function generateSiteMap(jobSlugs: string[] | null) {
+    let baseUrl = 'http://localhost:3000';
+    if (process.env.VERCEL_ENV) {
+        baseUrl = {
+            production: 'https://torchbox.com/careers',
+            preview: process.env.VERCEL_URL,
+            development: 'http://localhost:3000',
+        }[process.env.VERCEL_ENV] as string;
+    }
+
     return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <url>
-       <loc>${rootUrl}</loc>
-     </url>
-   </urlset>
- `;
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${baseUrl}</loc>
+      </url>
+      <url>
+        <loc>${baseUrl + '/life-at-torchbox/'}</loc>
+      </url>
+      <url>
+        <loc>${baseUrl + '/jobs/'}</loc>
+      </url>
+      ${
+          jobSlugs &&
+          jobSlugs.map(
+              (slug: string) => `
+              <url>
+                <loc>${baseUrl + slug}</loc>
+              </url>
+            `,
+          )
+      }
+    </urlset>
+  `;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const sitemap = generateSiteMap();
+    const jobSlugs = await getAllJobSlugs();
+    const sitemap = generateSiteMap(jobSlugs);
 
     context.res.setHeader('Content-Type', 'text/xml');
     context.res.write(sitemap);
