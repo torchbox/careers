26c01007e18821e35622ddc98f12b53cdb8b953a
Ben Morse <>
2022-02-23 11:01:07 +0000

    Generate sitemap pages dynamically

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,4 +1,6 @@
 import { GetServerSideProps } from 'next';
+import { ServerResponse } from 'http';
+import fs from 'fs';
 import { getAllJobSlugs } from './api/jobs/slugs';
 
 const generateSiteMap = (jobSlugs: string[] | null) => {
@@ -11,30 +13,52 @@ const generateSiteMap = (jobSlugs: string[] | null) => {
         }[process.env.VERCEL_ENV] as string;
     }
 
+    const staticPages = fs
+        .readdirSync(process.env.VERCEL_ENV ? './.next/server/pages' : 'pages')
+        .filter((staticPage) => {
+            // Filter out unwanted folders and pages
+            return ![
+                'api',
+                'jobs',
+                'index.tsx',
+                '_app.tsx',
+                '_error.js',
+                '_error.js',
+                'sitemap.xml.ts',
+            ].includes(staticPage);
+        })
+        .map((staticPagePath) => {
+            const path = `${baseUrl}/${staticPagePath}`.replace('.tsx', '');
+            return path;
+        });
+
     return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>${baseUrl}</loc>
       </url>
-      <url>
-        <loc>${baseUrl + '/life-at-torchbox'}</loc>
-      </url>
-      <url>
-        <loc>${baseUrl + '/jobs'}</loc>
-      </url>
+      ${staticPages
+          .map(
+              (url) => `
+          <url>
+            <loc>${url}</loc>
+          </url>
+        `,
+          )
+          .join('')}
       ${
           jobSlugs &&
           jobSlugs.map(
               (slug: string) => `
-              <url>
-                <loc>${baseUrl + slug}</loc>
-              </url>
-            `,
+          <url>
+            <loc>${baseUrl + slug}</loc>
+          </url>
+        `,
           )
       }
     </urlset>
   `;
-}
+};
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     const jobSlugs = await getAllJobSlugs();
