d0b7b0964bb7838796cb2e76bd73f544ba425b97
James Hancock <jhancock532@gmail.com>
2022-02-28 11:33:02 +0000

    refactor button to support coral variant

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,26 +2,61 @@ import Link from 'next/link';
 import { ChevronIcon } from '../Icons/ChevronIcon';
 import styles from './Button.module.scss';
 
+type ButtonTheme = 'INDIGO' | 'CORAL';
+
 type ButtonProps = {
     className?: string;
     url: string;
-    jobs: number;
+    jobs?: number;
+    internal?: boolean;
+    theme?: ButtonTheme;
     children: React.ReactNode;
 };
 
-export const Button = ({ className, url, jobs = 0, children }: ButtonProps) => (
-    <Link href={url}>
-        <a
-            className={[styles.button, styles.radialBackground, className].join(
-                ' ',
+export const Button = ({
+    className,
+    url,
+    jobs = 0,
+    internal = true,
+    theme = 'INDIGO',
+    children,
+}: ButtonProps) => {
+    const backgroundClass =
+        theme === 'INDIGO' ? styles.radialBackground : styles.coralBackground;
+
+    return (
+        <>
+            {internal ? (
+                <Link href={url}>
+                    <a
+                        className={[
+                            styles.button,
+                            backgroundClass,
+                            className,
+                        ].join(' ')}
+                    >
+                        {children}
+                        <ChevronIcon className={styles.chevron} />
+                        {jobs > 0 && (
+                            <div className={styles.jobsBadge}>{jobs}</div>
+                        )}
+                    </a>
+                </Link>
+            ) : (
+                <a
+                    className={[styles.button, backgroundClass, className].join(
+                        ' ',
+                    )}
+                    href={url}
+                >
+                    {children}
+                    <ChevronIcon className={styles.chevron} />
+                    {jobs > 0 && <div className={styles.jobsBadge}>{jobs}</div>}
+                </a>
             )}
-        >
-            {children}
-            <ChevronIcon className={styles.chevron} />
-            {jobs > 0 && <div className={styles.jobsBadge}>{jobs}</div>}
-        </a>
-    </Link>
-);
+        </>
+    );
+};
 
 export const SwishButton = ({
     className,
