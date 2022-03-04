3621fb1b880bc6d8584522812daebb865b987157
James Hancock <jhancock532@gmail.com>
2022-03-04 14:31:49 +0000

    add favicons to the page metadata

--- components/Metadata/Metadata.tsx
+++ components/Metadata/Metadata.tsx
@@ -39,7 +39,29 @@ export const Metadata = ({
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
         <meta property="twitter:image" content={image.url} />
-
+        <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+        />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+        />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="apple-mobile-web-app-title" content="Torchbox" />
+        <meta name="application-name" content="Torchbox" />
+        <meta name="msapplication-TileColor" content="#fd5765" />
+        <meta name="theme-color" content="#ffffff" />
         <link
             rel="schema.dcterms"
             href="https://dublincore.org/specifications/dublin-core/dcmi-terms/"
