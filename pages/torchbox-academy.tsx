e3e4c1683384d8cd1471df4041883b748f926cc6
James Hancock <jhancock532@gmail.com>
2022-03-04 10:35:08 +0000

    add conditional academies section and extra types

--- pages/torchbox-academy.tsx
+++ pages/torchbox-academy.tsx
@@ -3,6 +3,12 @@ import Layout from '../components/Layout';
 import { getTorchboxAcademyPage } from '../lib/api';
 import { TorchboxAcademy } from 'types/pages/TorchboxAcademy';
 import Academies from 'components/TorchboxAcademy/Academies';
+import type { AcademyTypes } from 'types/Base';
+
+type AcademyItemCollection = {
+    __typename: string;
+    academiesCollection: { items: AcademyTypes[] };
+};
 
 type TorchboxAcademyPageProps = {
     preview: boolean;
@@ -14,15 +20,17 @@ const TorchboxAcademyPage: NextPage<TorchboxAcademyPageProps> = ({
     content,
 }) => {
     const academyCollectionItem = content.itemsCollection.items.find(
-        (obj: any) => obj.__typename === 'Academies',
+        (obj: AcademyItemCollection) => obj.__typename === 'Academies',
     );
 
-    const academies = academyCollectionItem.academiesCollection.items;
+    let academies = undefined;
+    if (academyCollectionItem)
+        academies = academyCollectionItem.academiesCollection.items;
 
     return (
         <Layout theme="INDIGO" preview={preview} jobsAvailable={8}>
             <h1>Torchbox Academy</h1>
-            <Academies academies={academies} />
+            {academies && <Academies academies={academies} />}
         </Layout>
     );
 };
