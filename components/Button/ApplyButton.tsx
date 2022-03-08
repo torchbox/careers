1d066d014dcbd8062fffdc44465ffb34bb4020dd
James Hancock <jhancock532@gmail.com>
2022-03-08 15:18:04 +0000

    remove usescreen from sizing the chevron

--- components/Button/ApplyButton.tsx
+++ components/Button/ApplyButton.tsx
@@ -15,30 +15,20 @@ export const ApplyButton = ({
     url,
     title,
     children,
-}: ApplyButtonProps) => {
-    const [chevronSize, setChevronSize] = useState(18);
-    const screen = useScreen();
-
-    useEffect(() => {
-        if (screen.includes('medium')) setChevronSize(22);
-        if (screen.includes('large')) setChevronSize(24);
-    }, [screen]);
-
-    return (
-        <a
-            href={url}
-            className={[styles.button, styles.radialBackground, className].join(
-                ' ',
-            )}
-        >
-            <div className={styles.overflowHider}>
-                <div className={styles.swishBackground} />
-            </div>
-            <div className={styles.titleContainer}>
-                <p className={styles.title}>{title}</p>
-                <ChevronIcon className={styles.chevron} size={chevronSize} />
-            </div>
-            <div className={styles.text}>{children}</div>
-        </a>
-    );
-};
+}: ApplyButtonProps) => (
+    <a
+        href={url}
+        className={[styles.button, styles.radialBackground, className].join(
+            ' ',
+        )}
+    >
+        <div className={styles.overflowHider}>
+            <div className={styles.swishBackground} />
+        </div>
+        <div className={styles.titleContainer}>
+            <p className={styles.title}>{title}</p>
+            <ChevronIcon className={styles.chevron} />
+        </div>
+        <div className={styles.text}>{children}</div>
+    </a>
+);
