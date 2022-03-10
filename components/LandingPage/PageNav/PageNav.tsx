d10764643fac584872bce6400150fc90d5a9750c
James Hancock <jhancock532@gmail.com>
2022-03-10 16:14:01 +0000

    remove anchor links for homepage

--- components/LandingPage/PageNav/PageNav.tsx
+++ components/LandingPage/PageNav/PageNav.tsx
@@ -8,7 +8,7 @@ type PageNavProps = {
     children: React.ReactNode;
 };
 
-export const PageNav = ({ title, jobs = 0, children }: PageNavProps) => {
+const AnchorLinks = ({ jobs = 0 }: { jobs: number }) => {
     const smoothScrollToElement = (id: string) => {
         const element = document.querySelector(id);
         if (element) {
@@ -35,41 +35,44 @@ export const PageNav = ({ title, jobs = 0, children }: PageNavProps) => {
     };
 
     return (
+        <ul className={styles.navigation}>
+            <li className={styles.navItem}>
+                <a
+                    className={styles.navLink}
+                    onClick={() => smoothScrollToElement('#benefits')}
+                    onKeyDown={handleBenefitsLinkKeyDown}
+                    tabIndex={0}
+                >
+                    Benefits
+                </a>
+            </li>
+            <li className={styles.navItem}>
+                <a
+                    className={styles.navLink}
+                    onClick={() => smoothScrollToElement('#lifeAsATorchboxer')}
+                    onKeyDown={handleLifeAtTorchboxLinkKeyDown}
+                    tabIndex={0}
+                >
+                    Life as a Torchboxer
+                </a>
+            </li>
+            <li className={styles.jobNavItem}>
+                <Link href="/jobs" scroll={false}>
+                    <a className={styles.jobsLink}>
+                        Jobs
+                        {jobs > 0 ? (
+                            <div className={styles.jobsBadge}>{jobs}</div>
+                        ) : null}
+                    </a>
+                </Link>
+            </li>
+        </ul>
+    );
+};
+
+export const PageNav = ({ title, jobs = 0, children }: PageNavProps) => {
+    return (
         <div className={styles.container}>
-            <ul className={styles.navigation}>
-                <li className={styles.navItem}>
-                    <a
-                        className={styles.navLink}
-                        onClick={() => smoothScrollToElement('#benefits')}
-                        onKeyDown={handleBenefitsLinkKeyDown}
-                        tabIndex={0}
-                    >
-                        Benefits
-                    </a>
-                </li>
-                <li className={styles.navItem}>
-                    <a
-                        className={styles.navLink}
-                        onClick={() =>
-                            smoothScrollToElement('#lifeAsATorchboxer')
-                        }
-                        onKeyDown={handleLifeAtTorchboxLinkKeyDown}
-                        tabIndex={0}
-                    >
-                        Life as a Torchboxer
-                    </a>
-                </li>
-                <li className={styles.jobNavItem}>
-                    <Link href="/jobs" scroll={false}>
-                        <a className={styles.jobsLink}>
-                            Jobs
-                            {jobs > 0 ? (
-                                <div className={styles.jobsBadge}>{jobs}</div>
-                            ) : null}
-                        </a>
-                    </Link>
-                </li>
-            </ul>
             <ArrowLoopRight className={styles.image} />
             <h2 className={styles.title}>{title}</h2>
             <div className={styles.richText}>{children}</div>
