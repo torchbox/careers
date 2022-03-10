6bd97342b1424af78032eeae28655be3950a12cd
James Hancock <jhancock532@gmail.com>
2022-03-10 15:51:02 +0000

    fix favicon resources not loading

--- components/Metadata/Metadata.tsx
+++ components/Metadata/Metadata.tsx
@@ -42,22 +42,26 @@ export const Metadata = ({
         <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/careers/apple-touch-icon.png"
         />
         <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/careers/favicon-32x32.png"
         />
         <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/careers/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="manifest" href="/careers/site.webmanifest" />
+        <link
+            rel="mask-icon"
+            href="/careers/safari-pinned-tab.svg"
+            color="#5bbad5"
+        />
         <meta name="apple-mobile-web-app-title" content="Torchbox" />
         <meta name="application-name" content="Torchbox" />
         <meta name="msapplication-TileColor" content="#fd5765" />
