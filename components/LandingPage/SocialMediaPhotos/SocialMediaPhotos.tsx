cc93835fb7a0d7d6bd23fb5cb5520e2958bc1bce
James Hancock <jhancock532@gmail.com>
2022-03-24 15:58:00 +0000

    update social media photos as clickable links

--- components/LandingPage/SocialMediaPhotos/SocialMediaPhotos.tsx
+++ components/LandingPage/SocialMediaPhotos/SocialMediaPhotos.tsx
@@ -3,14 +3,28 @@ import { Image } from 'components/Image/Image';
 import styles from './SocialMediaPhotos.module.scss';
 
 type SocialMediaPhotosProps = {
-    photos: ImageTypes[];
+    photos: {
+        image: ImageTypes;
+        instagramLink: string;
+    }[];
 };
 
 export const SocialMediaPhotos = ({ photos }: SocialMediaPhotosProps) => {
-    const SocialMediaPhotos = photos.slice(0, 8).map((image, index) => (
-        <li className={styles.image} key={`social-media-image-${index}`}>
-            <Image layout="fill" src={image.url} alt={image.description} />
-        </li>
+    const SocialMediaPhotos = photos.slice(0, 8).map((photo, index) => (
+        <a
+            href={photo.instagramLink}
+            key={`social-media-image-${index}`}
+            target="_blank"
+            rel="noreferrer"
+        >
+            <li className={styles.image}>
+                <Image
+                    layout="fill"
+                    src={photo.image.url}
+                    alt={photo.image.description}
+                />
+            </li>
+        </a>
     ));
 
     return <ul className={styles.container}>{SocialMediaPhotos}</ul>;
