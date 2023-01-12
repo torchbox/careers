3ee50a21b414d42f8ee0fb72f20a066d12d18a52
James Hancock <james.hancock@torchbox.com>
2023-01-12 17:00:38 +0000

    fix Regex

--- components/YouTubeEmbed/YouTubeEmbed.tsx
+++ components/YouTubeEmbed/YouTubeEmbed.tsx
@@ -3,7 +3,7 @@ import styles from './YouTubeEmbed.module.scss';
 
 const getVideoId = (url: string) => {
     const regExp =
-        /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#&?]*).*/;
+        /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
     return match && match[2].length === 11 ? match[2] : null;
 };
