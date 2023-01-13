11070bb876c5cab14ed678cf6825d7dc37e2fc7a
James Hancock <james.hancock@torchbox.com>
2023-01-13 15:26:55 +0000

    remove Benefits section for independent trustee role

--- pages/jobs/[slug].tsx
+++ pages/jobs/[slug].tsx
@@ -77,10 +77,14 @@ const JobPosting: NextPage<JobPageProps> = ({
                 />
             </div>
 
-            <Benefits
-                title={content.itemsCollection.items[0].benefitsTitle}
-                benefits={benefits}
-            />
+            {/* Independent trustees don't receive benefits. Todo: This code change was requested as an ad-hoc fix -
+                we could pull a list of job titles that don't receive benefits from the CMS in the future. */}
+            {job.title !== 'Independent Trustee' && (
+                <Benefits
+                    title={content.itemsCollection.items[0].benefitsTitle}
+                    benefits={benefits}
+                />
+            )}
 
             <div className={styles.contentContainer}>
                 <div className={styles.textContainer}>
